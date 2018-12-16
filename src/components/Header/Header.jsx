098565484770273e9f0b2ca99dbfdd4861cd2521
Ina <inarb@stud.ntnu.no>
2018-12-16 18:13:29 +0100

    Fixed the links so that they are vertical. Need to add favorites as a link (instead of search?) Also added a serach field and icon. The items in the header are now displayed as a grid.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,21 +3,26 @@ import { Link } from "react-router-dom";
 
 import "./Header.scss";
 import logo from "../../assets/logo.svg";
+import searchIcon from "../../assets/005-search.png";
 
 const Header = () => (
   <header className="HeaderContainer">
     <img className="HeaderLogo" src={logo} alt="logo" />
     <h1 className="HeaderHeading">B-Line</h1>
-    <nav>
+    <nav className="LinkContainer">
       <ul>
         <li>
-          <Link to="/">Search</Link>
+          <Link className="HeaderLink" to="/">Search</Link>
         </li>
         <li>
-          <Link to="/settings">Settings</Link>
+          <Link className="HeaderLink" to="/settings">Settings</Link>
         </li>
       </ul>
     </nav>
+    <div className="Search">
+      <img className="fa fa-icon" src={searchIcon} alt="searchIcon" />
+      <input type="text" name="search" placeholder="What can we help you with?" />
+    </div> 
   </header>
 );
 
