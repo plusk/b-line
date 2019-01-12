c53b5d2088995f2602783102d273883a75b480a4
hffmiklin <hannah.miklin@gmail.com>
2019-01-12 16:19:39 +0100

    Profile picture upload and drag and drop

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,6 +2,17 @@
   <div class="wrapper">
     <div class="container">
       <h2>Settings</h2>
+
+      <!-- drag n drop / upload image -->
+      <div class="droparea display-inline align-center" @dragover.prevent @drop="onDrop">
+        <label v-if="!image" class="btn display-inline">
+          Select or drop image
+          <input type="file" name="image" @change="onChange">
+        </label>
+        <div class="hidden display-inline align-center" v-else v-bind:class="{ 'image': true }">
+          <img :src="image" alt class="img">
+        </div>
+      </div>
       <div class="setting">
         <h3>{{title}}</h3>
         <div class="radios">
@@ -21,6 +32,8 @@
 </template>
 
 <script>
+import userIcon from "@/assets/user_img.png";
+
 export default {
   data: function() {
     return {
@@ -31,14 +44,42 @@ export default {
         "Fewest transfers",
         "Least walking"
       ],
-      selected: ""
+      selected: "",
+      image: ""
     };
   },
   mounted: function() {
     this.selected = this.values[1];
+  },
+  methods: {
+    onDrop: function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      var files = e.dataTransfer.files;
+      this.createFile(files[0]);
+    },
+    onChange(e) {
+      var files = e.target.files;
+      this.createFile(files[0]);
+    },
+    createFile(file) {
+      if (!file.type.match("image.*")) {
+        alert("Select an image");
+        return;
+      }
+      var img = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = function(e) {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
   }
 };
 </script>
+
 
 <style lang="scss" scoped>
 @import "@/common/constants.scss";
@@ -54,6 +95,12 @@ export default {
   }
 }
 
+label.btn {
+  font-size: 10px;
+  display: table-cell;
+  vertical-align: middle;
+}
+
 .radios {
   margin-top: 10px;
   display: flex;
@@ -62,5 +109,68 @@ export default {
   & > * + * {
     margin-top: 10px;
   }
+}
+
+.img {
+  max-height: 25%;
+  max-width: 25%;
+  border-radius: 50%;
+  margin-bottom: 20px;
+}
+/*
+.droparea {
+  border-radius: 50%;
+  height: 100%;
+  max-height: 400px;
+  max-width: 400px;
+} */
+
+/* background-color: #f3c96d;
+  border-radius: 50%;
+  width: 10%;
+  max-height: 40%;
+  max-width: 40%;
+  color: #fff;
+  cursor: pointer;
+  padding: 28px 45px;
+  margin-bottom: 20px;  */
+
+.btn {
+  background-color: #f3c96d;
+  border-radius: 50%;
+  display: block;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  color: #fff;
+  cursor: pointer;
+  margin-bottom: 20px;
+}
+
+.btn:hover {
+  background-color: #ffa42d;
+}
+
+input[type="file"] {
+  position: absolute;
+  opacity: 0;
+  z-index: -1;
+}
+
+.align-center {
+  text-align: center;
+}
+
+.hidden {
+  display: none !important;
+}
+
+.hidden.image {
+  display: inline-block !important;
+}
+
+.display-inline {
+  display: inline-block;
+  vertical-align: middle;
 }
 </style>
